a4ab217489c10e3bf80a56406cd1b49c0086ee7a
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-03-13 09:01:17 +0100

    version 1.2

--- src/main/java/com/ashkiano/itemspawn/ItemSpawn.java
+++ src/main/java/com/ashkiano/itemspawn/ItemSpawn.java
@@ -10,7 +10,7 @@ public final class ItemSpawn extends JavaPlugin {
         this.saveDefaultConfig(); // Zajistí, že máme config.yml
         getCommand("spawnitem").setExecutor(new ItemSpawnCommandExecutor(this));
         new ItemSpawnLoader(this).loadAllSpawns(); // Načte a aktivuje všechny uložené spawn pointy
-        getLogger().info("ItemSpawnPlugin has been enabled!");
+        this.getLogger().info("Thank you for using the ItemSpawn plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
         Metrics metrics = new Metrics(this, 21215);
     }
 
